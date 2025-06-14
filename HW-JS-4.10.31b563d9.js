ce4d0283c24b79cfc2c8da4a78baca7d31af6799
Timka09 <tymofij.i.naumenko@gmail.com>
2025-06-15 00:45:36 +0300

    Updates

--- HW-JS-4.10.31b563d9.js
+++ HW-JS-4.10.31b563d9.js
@@ -667,6 +667,67 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"a0t4e":[function(require,module,exports,__globalThis) {
+// task 1
+const getString = document.querySelector(".test");
+let acc = 0;
+const interval = setInterval(()=>{
+    getString.innerHTML = acc++;
+    if (acc === 6) clearInterval(interval);
+}, 1000);
+// task 2
+const getTitle = document.querySelector(".word");
+const getBox = document.querySelector(".box2");
+let scale = 1;
+const maxScale = 2.5;
+const step = 0.5;
+const int2 = setInterval(()=>{
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    getTitle.style.color = `rgb(${r}, ${g}, ${b})`;
+    if (scale < maxScale) {
+        scale += step;
+        getBox.style.transform = `scale(${scale})`;
+    }
+}, 1000);
+// task 3
+const getBtn = document.querySelector(".btn");
+const getStartBtn = document.querySelector(".start-btn");
+const getCount = document.querySelector(".count");
+const getTime = document.querySelector(".time");
+let sec = 0;
+let score = 0;
+let int3;
+function startGame() {
+    clearInterval(int3);
+    sec = 30;
+    score = 0;
+    getTime.textContent = sec;
+    getCount.textContent = score;
+    int3 = setInterval(()=>{
+        sec -= 1;
+        getTime.textContent = sec;
+        if (sec <= 0) clearInterval(int3);
+    }, 1000);
+}
+function gameClick() {
+    score += 1;
+    getCount.textContent = score;
+}
+getStartBtn.addEventListener("click", startGame);
+getBtn.addEventListener("click", gameClick);
+// task4
+const getBtnStart = document.querySelector(".time-start-btn");
+const getInput = document.querySelector(".time-input");
+const getMessage = document.querySelector(".message4");
+function startTime() {
+    const secondsInput = Number(getInput.value);
+    getMessage.textContent = "\u041E\u0447\u0456\u043A\u0443\u0454\u043C\u043E..";
+    setTimeout(()=>{
+        getMessage.textContent = `\u{41F}\u{440}\u{43E}\u{439}\u{448}\u{43B}\u{43E} ${secondsInput} \u{441}\u{435}\u{43A}\u{443}\u{43D}\u{434}`;
+    }, secondsInput * 1000);
+}
+getBtnStart.addEventListener("click", startTime);
 
 },{}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequireabde", {})
 
--- HW-JS-4.10.b6fe3731.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=HW-JS-4.10.b6fe3731.js.map
